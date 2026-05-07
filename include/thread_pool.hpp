460aab528c3095f4d90e71abc66a191b24bcb28e
JeanCassiano <jean_cassiano_@hotmail.com>
2026-05-07 16:35:38 +0200

    feat: Added header file

---
+++ include/thread_pool.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+
+
+class ThreadPool {
+public:
+    explicit ThreadPool(size_t num_threads);
+    ~ThreadPool();
+    void enqueue(std::function<void()> task);
+private:
+    void worker_loop();
+
+    std::vector<std::thread>            workers;
+    std::queue<std::function<void()>>   tasks;
+    std::mutex                          queue_mutex;
+    std::condition_variable             condition;
+    bool                                stop = false;
+};
+
+inline ThreadPool::ThreadPool(size_t num_threads){
+    for (size_t i = 0 ; i < num_threads; i++){
+        workers.emplace_back(&ThreadPool::worker_loop, this);
+    }
+}
+
